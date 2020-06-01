8f1f36d570b8965af85e9d6f8a2d9ce28f8e42c4
Baron <36326959+Baron010@users.noreply.github.com>
2020-06-01 14:08:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const puppeteer = require('puppeteer');
-const { BOTS, COOKIE } = require('./config.json');
+const { BOT_ID, BOT_NAME, COOKIE } = require('./config.json');
 const logger = require('./logger.js');
 
-if (!BOTS) return logger.err('Config BOTS is missing');
+if (!BOT_ID) return logger.err('Config BOT_ID is missing');
+if (!BOT_NAME) return logger.err('Config BOT_NAME is missing');
 if (!COOKIE) return logger.err('Config COOKIE is missing');
 
-function vote() {
-	BOTS.forEach(async b => {
-	logger.try(`Trying to vote for ${b.name}...`);
+async function vote() {
+	logger.try(`Trying to vote for ${BOT_NAME}...`);
 
 	const browser = await puppeteer.launch({ headless: false, args: ['--window-size=0,300'] });
 	const page = (await browser.pages())[0];
 
 	await page.setCookie({ name: 'connect.sid', value: COOKIE, domain: 'top.gg', path: '/' });
-	await page.goto(`https://top.gg/bot/${b.id}/vote`);
-	
-	setTimeout(() => {
-			 page.click('#votingvoted');
-		},10000);
-			setTimeout(() => {
-				logger.succ(`Successfully voted for ${b.name}!`);
-				browser.close()
-			},15000);
+	await page.goto(`https://top.gg/bot/${BOT_ID}/vote`);
 
-			if (b.id == BOTS[BOTS.length - 1].id) {
-				setTimeout(() => {
-					setTimeout(vote, 1000 * 60 * 60 * 12);
-					logger.info('Waiting 12 hours');
-				}, 5000);
-			}
-		});
+	await new Promise((resolve)=>setTimeout(() => {
+		page.click('#votingvoted');
+        resolve();
+	}, 10000));
+
+	await new Promise((resolve)=>setTimeout(() => {
+		logger.succ(`Successfully voted for ${BOT_NAME}!`);
+		browser.close()
+		logger.info('Waiting 12 hours');
+        resolve();
+	}, 2000));
 }
 vote();
+
+setInterval(() => {
+	vote();
+}, 1000 * 60 * 60 * 12);
